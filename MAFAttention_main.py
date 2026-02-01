bb21c1f8b6cd40fb84972b9eaa9b82c770f76ff6
_mqt. <23133071@student.hcmute.edu.vn>
2026-02-01 22:26:15 +0700

    Update MAFAttention_main.py

--- MAFAttention_main.py
+++ MAFAttention_main.py
@@ -215,7 +215,7 @@ def main():
         if not os.path.exists(pathout):
             os.makedirs(pathout)
         # get model
-        model = build_mobilenet_v2(120, width_multiplier=1.0, cifar=False,pool_types=['avg', 'max'])
+        model = build_mobilenet_v3(120, "large", width_multiplier=1, cifar=False,use_lightweight_head=False,pool_types=['avg', 'max'])
         model = model.to(device)
 
         print(model)
